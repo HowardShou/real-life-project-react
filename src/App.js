2ab52e05d3778326fefc4a348cd12404552d084d
HowardShou <howard@innotech.me>
2021-01-29 22:32:06 +0800

    temporary layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css'
+import { useState } from 'react'
+import './App.scss'
 import ListTest from 'components/ListTest'
+import CreditCardForm from 'components/CreditCardForm'
 
 function App() {
+  const [state] = useState('1')
   return (
-    <div className='App'>
-      <ListTest />
+    <div id='App'>
+      <div className='nav'>navnav</div>
+      <div className='container'>
+        {state === '0' ? <ListTest /> : null}
+        {state === '1' ? <CreditCardForm /> : null}
+      </div>
     </div>
   )
 }
