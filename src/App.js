eb8d9d0a57deade0b962a042ed2e83f6b6a1d4a5
HowardShou <howard@innotech.me>
2021-02-02 15:17:45 +0800

    temp app files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import ListTest from 'components/ListTest'
 import CreditCardForm from 'components/CreditCardForm'
 
 function App() {
-  const [state] = useState('1')
+  const [state, setState] = useState('1')
   return (
     <div id='App'>
-      <div className='nav'>navnav</div>
+      <div className='nav'>
+        <div>navnav</div>
+        <button onClick={() => setState('0')}>change ListTest</button>
+        <button onClick={() => setState('1')}>change CreditCardForm</button>
+      </div>
       <div className='container'>
         {state === '0' ? <ListTest /> : null}
         {state === '1' ? <CreditCardForm /> : null}
