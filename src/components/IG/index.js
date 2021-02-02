7a2d3564d56751b03dfa752aa42c4f1ebe0b5294
HowardShou <a95674124@gmail.com>
2021-02-02 18:08:16 +0800

    fetch json test

--- src/components/IG/index.js
+++ src/components/IG/index.js
@@ -2,11 +2,25 @@ import { useState } from 'react'
 
 const Image = () => {
   const [pic, setPic] = useState('')
+  const [data, setData] = useState('')
 
-  const handleClick = () => {
+  const handleClick1 = () => {
     const fetchData = async () => {
       const res = await fetch('https://picsum.photos/500/500')
       if (res.ok) setPic(res.url)
+    }
+    fetchData()
+  }
+
+  const handleClick2 = () => {
+    const fetchData = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      let data
+      if (res.ok) {
+        data = await res.json()
+        console.log('🚹🚺🚻🛏️🚼 ~ file: index.js ~ line 21 ~ fetchData ~ data', data)
+        setData(JSON.stringify(data))
+      }
     }
     fetchData()
   }
@@ -16,9 +30,11 @@ const Image = () => {
   //   }, [])
   return (
     <div>
-      <button onClick={handleClick}>fetch</button>
+      <button onClick={handleClick1}>fetch img</button>
+      <button onClick={handleClick2}>fetch json placeholder</button>
       <div>
         <img src={pic} />
+        <div>{data}</div>
       </div>
     </div>
   )
