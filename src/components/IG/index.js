340af7a42b4bcaabed65faaad766364f880dd2b0
HowardShou <a95674124@gmail.com>
2021-02-05 20:51:14 +0800

    image component update

--- src/components/IG/index.js
+++ src/components/IG/index.js
@@ -1,43 +1,103 @@
-import { useState } from 'react'
+import { useState, useMemo, useEffect } from 'react'
+import useSWR from 'swr'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import Image from 'material-ui-image'
 
-const Image = () => {
-  const [pic, setPic] = useState('')
-  const [data, setData] = useState('')
+function getRandom(x) {
+  return Math.floor(Math.random() * x) + 1
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+}))
+
+const Images = (props) => {
+  const [page, setPage] = useState(1)
+  const [picUrl, setPicUrl] = useState(`https://picsum.photos/v2/list?page=${page}`)
+  const [jsonUrl, setJsonUrl] = useState(null)
+  const classes = useStyles()
+
+  const { data: jsonArr } = useSWR(jsonUrl)
+  const { data: pics } = useSWR(picUrl)
+
+  const formatPics = useMemo(
+    () =>
+      pics?.map((detail) => {
+        const [protocol, domainStr] = detail.download_url.split('//')
+        const domainArr = domainStr.split('/')
+        let [width, height] = domainArr.splice(-2, 2)
+        width = 200
+        height = 200
+        const newUrl = `${protocol}//${domainArr.join('/')}/${width}/${height}`
+        return { ...detail, download_url: newUrl }
+      }),
+    [pics]
+  )
 
   const handleClick1 = () => {
-    const fetchData = async () => {
-      const res = await fetch('https://picsum.photos/500/500')
-      if (res.ok) setPic(res.url)
-    }
-    fetchData()
+    setPage(getRandom(30))
   }
 
   const handleClick2 = () => {
-    const fetchData = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-      let data
-      if (res.ok) {
-        data = await res.json()
-        console.log('🚹🚺🚻🛏️🚼 ~ file: index.js ~ line 21 ~ fetchData ~ data', data)
-        setData(JSON.stringify(data))
-      }
-    }
-    fetchData()
+    setJsonUrl('https://jsonplaceholder.typicode.com/todos')
   }
 
-  //   useEffect(() => {
+  useEffect(() => {
+    setPicUrl(`https://picsum.photos/v2/list?page=${page}`)
+  }, [page])
 
-  //   }, [])
   return (
     <div>
-      <button onClick={handleClick1}>fetch img</button>
-      <button onClick={handleClick2}>fetch json placeholder</button>
-      <div>
-        <img src={pic} />
-        <div>{data}</div>
+      <Button variant='contained' color='primary' onClick={handleClick1}>
+        Fetch Image
+      </Button>
+      <Button variant='secondary' color='primary' onClick={handleClick2}>
+        fetch data
+      </Button>
+      <Button
+        variant='secondary'
+        color='primary'
+        onClick={() => {
+          if (page < 30) return setPage((c) => ++c)
+        }}
+      >
+        +1
+      </Button>
+      <Button
+        variant='secondary'
+        color='primary'
+        onClick={() => {
+          if (page > 1) return setPage((c) => --c)
+        }}
+      >
+        -1
+      </Button>
+      <div className={classes.root}>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+          {formatPics?.map((pic) => (
+            <GridListTile key={pic.download_url} cols={pic.cols || 1}>
+              <Image src={pic.download_url} alt={pic.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+        {/* <div>{pics && pics.map((item, idx) => <img key={idx} src={item.download_url} />)}</div> */}
+        {jsonArr ? JSON.stringify(jsonArr) : null}
       </div>
     </div>
   )
 }
 
-export default Image
+export default Images
