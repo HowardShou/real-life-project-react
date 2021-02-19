eb0c2a62096160a986de9b1b660efa1c191fd83c
HowardShou <a95674124@gmail.com>
2021-02-19 18:48:59 +0800

    add custom hook- useDynamicHeaderHeight.js

---
+++ src/hooks/useDynamicHeaderHeight.js
@@ -0,0 +1,25 @@
+import { useMemo } from 'react'
+import DYNAMIC_MINUS_HEIGHT from 'constants/dynamicMinusHeight'
+import { useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useLocation } from 'react-router-dom'
+
+const useDynamicHeaderHeight = () => {
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('xs'))
+  let location = useLocation()
+
+  const headerHeight = useMemo(() => {
+    let headerHeight = 0
+    if (location.pathname.includes('/Demos') && matches) headerHeight = DYNAMIC_MINUS_HEIGHT.DEMO_AND_MOBILE
+    else if (location.pathname.includes('/Demos') && !matches) headerHeight = DYNAMIC_MINUS_HEIGHT.DEMO_BUT_NOT_MOBILE
+    else if (!location.pathname.includes('/Demos') && matches) headerHeight = DYNAMIC_MINUS_HEIGHT.NOT_DEMO_BUT_MOBILE
+    else if (!location.pathname.includes('/Demos') && !matches)
+      headerHeight = DYNAMIC_MINUS_HEIGHT.NOT_DEMO_AND_NOT_MOBILE
+    return headerHeight
+  }, [location.pathname, matches])
+
+  return headerHeight
+}
+
+export default useDynamicHeaderHeight
